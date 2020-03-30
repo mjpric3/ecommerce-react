6ffe741ff7958d22e09274eb516a6e041d460be2
MICHAEL PRICE <mjpric3@gmail.com>
2020-03-29 23:18:12 -0500

    added page navigation

--- REACT_JS/ecommerce/src/components/directory/directory.component.jsx
+++ REACT_JS/ecommerce/src/components/directory/directory.component.jsx
@@ -12,44 +12,49 @@ constructor() {
             {
                 title: 'Hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'Jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2
+                id: 2,
+                linkUrl: 'jackets'
             },
             {
                 title: 'Sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'Womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 'Mens',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }]
     }
 }
 
-render() {
-    return(
-        <div className="directory-menu">
-            {this.state.sections.map(
-                ({id, title, imageUrl, size}) => (
-                    <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
-                )
-            )}
-        </div>
-    )
-}
+    render() {
+        return(
+            <div className="directory-menu">
+                {this.state.sections.map(
+                    ({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
+                    )
+                )}
+            </div>
+        )
+    }
 
 }
 
